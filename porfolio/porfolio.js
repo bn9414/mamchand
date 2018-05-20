76975538a07ae7a3481a0e942fe7d37fd571659b
Bhuvanesh <bhuvaneshnick@gmail.com>
2018-05-20 08:36:01 +0530

    s

---
+++ porfolio/porfolio.js
@@ -0,0 +1,492 @@
+var dot1 = document.getElementById("dot1");
+var dot2 = document.getElementById("dot2");
+
+dot1.addEventListener("click",function(){
+                      slde(1);},false);
+
+dot2.addEventListener("click",function(){
+                      slde(2);},false);
+
+function slde(a){
+    console.log(a);
+    if(a == 1){
+        dot1.style.background="#ffcc33";
+        dot1.style.border="1px solid #ffcc33";
+        
+        dot2.style.background="#454545 ";
+        dot2.style.border="1px solid #454545";
+        
+        var img1 = document.getElementsByClassName('img1');
+        img1[0].style.display="none";
+        img1[1].style.display="block";
+        
+    }
+    else if(a == 2){
+        dot2.style.background="#ffcc33";
+        dot2.style.border="1px solid #ffcc33";
+        
+        dot1.style.background="#454545";
+        dot1.style.border="1px solid #454545";
+         var img2 = document.getElementsByClassName('img1');
+        img2[0].style.display="block";
+        img2[1].style.display="none";
+    }
+}
+slde(1);
+
+
+var dot3 = document.getElementById("dot3");
+var dot4 = document.getElementById("dot4");
+
+dot3.addEventListener("click",function(){
+                      slde1(1);},false);
+
+dot4.addEventListener("click",function(){
+                      slde1(2);},false);
+
+function slde1(b){
+   
+    if(b == 1){
+        dot3.style.background="#ffcc33";
+        dot3.style.border="1px solid #ffcc33";
+        
+        dot4.style.background="#454545 ";
+        dot4.style.border="1px solid #454545";
+        
+        var img2 = document.getElementsByClassName('img2');
+        img2[0].style.display="none";
+        img2[1].style.display="block";
+        
+    }
+    else if(b == 2){
+        dot4.style.background="#ffcc33";
+        dot4.style.border="1px solid #ffcc33";
+        
+        dot3.style.background="#454545";
+        dot3.style.border="1px solid #454545";
+         var img21 = document.getElementsByClassName('img2');
+        img21[0].style.display="block";
+        img21[1].style.display="none";
+    }
+}
+slde1(1);
+
+
+
+var dot5 = document.getElementById("dot5");
+var dot6 = document.getElementById("dot6");
+
+dot5.addEventListener("click",function(){
+                      slde2(1);},false);
+
+dot6.addEventListener("click",function(){
+                      slde2(2);},false);
+
+function slde2(c){
+   
+    if(c == 1){
+        dot5.style.background="#ffcc33";
+        dot5.style.border="1px solid #ffcc33";
+        
+        dot6.style.background="#454545 ";
+        dot6.style.border="1px solid #454545";
+        
+        var img3 = document.getElementsByClassName('img3');
+        img3[0].style.display="none";
+        img3[1].style.display="block";
+        
+    }
+    else if(c == 2){
+        dot6.style.background="#ffcc33";
+        dot6.style.border="1px solid #ffcc33";
+        
+        dot5.style.background="#454545";
+        dot5.style.border="1px solid #454545";
+         var img22 = document.getElementsByClassName('img3');
+        img22[0].style.display="block";
+        img22[1].style.display="none";
+    }
+}
+slde2(1);
+
+var dot7 = document.getElementById("dot7");
+var dot8 = document.getElementById("dot8");
+
+dot7.addEventListener("click",function(){
+                      slde3(1);},false);
+
+dot8.addEventListener("click",function(){
+                      slde3(2);},false);
+
+function slde3(d){
+   
+    if(d == 1){
+        dot7.style.background="#ffcc33";
+        dot7.style.border="1px solid #ffcc33";
+        
+        dot8.style.background="#454545 ";
+        dot8.style.border="1px solid #454545";
+        
+        var img4 = document.getElementsByClassName('img4');
+        img4[0].style.display="none";
+        img4[1].style.display="block";
+        
+    }
+    else if(d == 2){
+        dot8.style.background="#ffcc33";
+        dot8.style.border="1px solid #ffcc33";
+        
+        dot7.style.background="#454545";
+        dot7.style.border="1px solid #454545";
+         var img24 = document.getElementsByClassName('img4');
+        img24[0].style.display="block";
+        img24[1].style.display="none";
+    }
+}
+slde3(1);
+
+var dot9 = document.getElementById("dot9");
+var dot10 = document.getElementById("dot10");
+
+dot9.addEventListener("click",function(){
+                      slde4(1);},false);
+
+dot10.addEventListener("click",function(){
+                      slde4(2);},false);
+
+function slde4(e){
+   
+    if(e == 1){
+        dot9.style.background="#ffcc33";
+        dot9.style.border="1px solid #ffcc33";
+        
+        dot10.style.background="#454545 ";
+        dot10.style.border="1px solid #454545";
+        
+        var img5 = document.getElementsByClassName('img5');
+        img5[0].style.display="none";
+        img5[1].style.display="block";
+        
+    }
+    else if(e == 2){
+        dot10.style.background="#ffcc33";
+        dot10.style.border="1px solid #ffcc33";
+        
+        dot9.style.background="#454545";
+        dot9.style.border="1px solid #454545";
+         var img25 = document.getElementsByClassName('img5');
+        img25[0].style.display="block";
+        img25[1].style.display="none";
+    }
+}
+slde4(1);
+
+var dot11 = document.getElementById("dot11");
+var dot12 = document.getElementById("dot12");
+
+dot11.addEventListener("click",function(){
+                      slde5(1);},false);
+
+dot12.addEventListener("click",function(){
+                      slde5(2);},false);
+
+function slde5(f){
+   
+    if(f == 1){
+        dot11.style.background="#ffcc33";
+        dot11.style.border="1px solid #ffcc33";
+        
+        dot12.style.background="#454545 ";
+        dot12.style.border="1px solid #454545";
+        
+        var img6 = document.getElementsByClassName('img6');
+        img6[0].style.display="none";
+        img6[1].style.display="block";
+        
+    }
+    else if(f == 2){
+        dot12.style.background="#ffcc33";
+        dot12.style.border="1px solid #ffcc33";
+        
+        dot11.style.background="#454545";
+        dot11.style.border="1px solid #454545";
+         var img65 = document.getElementsByClassName('img6');
+        img65[0].style.display="block";
+        img65[1].style.display="none";
+    }
+}
+slde5(1);
+
+var dot13 = document.getElementById("dot13");
+var dot14 = document.getElementById("dot14");
+
+dot13.addEventListener("click",function(){
+                      slde6(1);},false);
+
+dot14.addEventListener("click",function(){
+                      slde6(2);},false);
+
+function slde6(g){
+   
+    if(g == 1){
+        dot13.style.background="#ffcc33";
+        dot13.style.border="1px solid #ffcc33";
+        
+        dot14.style.background="#454545 ";
+        dot14.style.border="1px solid #454545";
+        
+        var img7 = document.getElementsByClassName('img7');
+        img7[0].style.display="none";
+        img7[1].style.display="block";
+        
+    }
+    else if(g == 2){
+        dot14.style.background="#ffcc33";
+        dot14.style.border="1px solid #ffcc33";
+        
+        dot13.style.background="#454545";
+        dot13.style.border="1px solid #454545";
+         var img75 = document.getElementsByClassName('img7');
+        img75[0].style.display="block";
+        img75[1].style.display="none";
+    }
+}
+slde6(1);
+
+var dot15 = document.getElementById("dot15");
+var dot16 = document.getElementById("dot16");
+
+dot15.addEventListener("click",function(){
+                      slde7(1);},false);
+
+dot16.addEventListener("click",function(){
+                      slde7(2);},false);
+
+function slde7(g){
+   
+    if(g == 1){
+        dot15.style.background="#ffcc33";
+        dot15.style.border="1px solid #ffcc33";
+        
+        dot16.style.background="#454545 ";
+        dot16.style.border="1px solid #454545";
+        
+        var img8 = document.getElementsByClassName('img8');
+        img8[0].style.display="none";
+        img8[1].style.display="block";
+        
+    }
+    else if(g == 2){
+        dot16.style.background="#ffcc33";
+        dot16.style.border="1px solid #ffcc33";
+        
+        dot15.style.background="#454545";
+        dot15.style.border="1px solid #454545";
+         var img85 = document.getElementsByClassName('img8');
+        img85[0].style.display="block";
+        img85[1].style.display="none";
+    }
+}
+slde7(1);
+
+var dot17 = document.getElementById("dot17");
+var dot18 = document.getElementById("dot18");
+
+dot17.addEventListener("click",function(){
+                      slde8(1);},false);
+
+dot18.addEventListener("click",function(){
+                      slde8(2);},false);
+
+function slde8(g){
+   
+    if(g == 1){
+        dot17.style.background="#ffcc33";
+        dot17.style.border="1px solid #ffcc33";
+        
+        dot18.style.background="#454545 ";
+        dot18.style.border="1px solid #454545";
+        
+        var img9 = document.getElementsByClassName('img9');
+        img9[0].style.display="none";
+        img9[1].style.display="block";
+        
+    }
+    else if(g == 2){
+        dot18.style.background="#ffcc33";
+        dot18.style.border="1px solid #ffcc33";
+        
+        dot17.style.background="#454545";
+        dot17.style.border="1px solid #454545";
+         var img95 = document.getElementsByClassName('img9');
+        img95[0].style.display="block";
+        img95[1].style.display="none";
+    }
+}
+slde8(1);
+
+var dot19 = document.getElementById("dot19");
+var dot20 = document.getElementById("dot20");
+
+dot19.addEventListener("click",function(){
+                      slde9(1);},false);
+
+dot20.addEventListener("click",function(){
+                      slde9(2);},false);
+
+function slde9(g){
+   
+    if(g == 1){
+        dot19.style.background="#ffcc33";
+        dot19.style.border="1px solid #ffcc33";
+        
+        dot20.style.background="#454545 ";
+        dot20.style.border="1px solid #454545";
+        
+        var img10 = document.getElementsByClassName('img10');
+        img10[0].style.display="none";
+        img10[1].style.display="block";
+        
+    }
+    else if(g == 2){
+        dot20.style.background="#ffcc33";
+        dot20.style.border="1px solid #ffcc33";
+        
+        dot19.style.background="#454545";
+        dot19.style.border="1px solid #454545";
+        
+         var img105 = document.getElementsByClassName('img10');
+        img105[0].style.display="block";
+        img105[1].style.display="none";
+    }
+}
+slde9(1);
+
+var manage = document.getElementsByClassName('manage');
+var man1 = document.getElementById('man1');
+var man2 = document.getElementById('man2');
+var man3 = document.getElementById('man3');
+var man4 = document.getElementById('man4');
+var man5 = document.getElementById('man5');
+
+man1.addEventListener('click',function(){
+    clcky(1)
+},false);
+
+man2.addEventListener('click',function(){
+    clcky(2)
+},false);
+
+man3.addEventListener('click',function(){
+    clcky(3)
+},false);
+
+man4.addEventListener('click',function(){
+    clcky(4)
+},false);
+
+man5.addEventListener('click',function(){
+    clcky(5)
+},false);
+
+var actab = document.getElementsByClassName('actab');
+
+function clcky(g){
+    if(g == 1){
+        
+        manage[0].style.display="flex";
+        manage[1].style.display="flex";
+        manage[2].style.display="flex";
+        manage[3].style.display="flex";
+        manage[4].style.display="flex";
+        manage[5].style.display="flex";
+        manage[6].style.display="flex";
+        manage[7].style.display="flex";
+        manage[8].style.display="flex";
+        manage[9].style.display="flex";
+        
+        
+        actab[0].style.opacity="1";
+        actab[1].style.opacity="0";
+        actab[2].style.opacity="0";
+        actab[3].style.opacity="0";
+        actab[4].style.opacity="0";
+        
+    }
+    if(g == 2){
+        
+        manage[0].style.display="none";
+        manage[1].style.display="flex";
+        manage[2].style.display="flex";
+        manage[3].style.display="flex";
+        manage[4].style.display="flex";
+        manage[5].style.display="flex";
+        manage[6].style.display="flex";
+        manage[7].style.display="flex";
+        manage[8].style.display="flex";
+        manage[9].style.display="none";
+        
+        
+        actab[0].style.opacity="0";
+        actab[1].style.opacity="1";
+        actab[2].style.opacity="0";
+        actab[3].style.opacity="0";
+        actab[4].style.opacity="0";
+        
+    }
+    if(g == 3){
+         manage[0].style.display="none";
+        manage[1].style.display="none";
+        manage[2].style.display="none";
+        manage[3].style.display="none";
+        manage[4].style.display="none";
+        manage[5].style.display="none";
+        manage[6].style.display="none";
+        manage[7].style.display="none";
+        manage[8].style.display="none";
+        manage[9].style.display="flex";
+        
+        actab[0].style.opacity="0";
+        actab[1].style.opacity="0";
+        actab[2].style.opacity="1";
+        actab[3].style.opacity="0";
+        actab[4].style.opacity="0";
+        
+    }
+    if(g == 4){
+         manage[0].style.display="flex";
+        manage[1].style.display="none";
+        manage[2].style.display="none";
+        manage[3].style.display="none";
+        manage[4].style.display="none";
+        manage[5].style.display="none";
+        manage[6].style.display="none";
+        manage[7].style.display="none";
+        manage[8].style.display="none";
+        manage[9].style.display="none";
+        
+        actab[0].style.opacity="0";
+        actab[1].style.opacity="0";
+        actab[2].style.opacity="0";
+        actab[3].style.opacity="1";
+        actab[4].style.opacity="0";
+        
+    }
+    if(g == 5){
+        manage[0].style.display="none";
+        manage[1].style.display="flex";
+        manage[2].style.display="flex";
+        manage[3].style.display="none";
+        manage[4].style.display="flex";
+        
+        actab[0].style.opacity="0";
+        actab[1].style.opacity="0";
+        actab[2].style.opacity="0";
+        actab[3].style.opacity="0";
+        actab[4].style.opacity="1";
+         
+    }
+}
+clcky(1);
+
+
